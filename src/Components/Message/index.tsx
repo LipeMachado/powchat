0556929f98d45516e6ad5699db1baabffd817196
LipeMachado <silvalipe765@gmail.com>
2022-10-18 17:34:39 -0300

    Added Message

--- src/Components/Message/index.tsx
+++ src/Components/Message/index.tsx
@@ -1,13 +1,46 @@
 import React from 'react'
 import {
-    MessageContainerDiv
+    MessageContainerDiv,
+    MessageInfoDiv,
+    MessageContentDiv
 } from './style'
+
+import ImageExample from '../../Assets/ImageExample.jpg'
 
 export function Message() {
     return (
         <>
-            <MessageContainerDiv>
-                Message
+            <MessageContainerDiv className="owner">
+                <MessageInfoDiv>
+                    <img
+                        src={ImageExample}
+                        alt=""
+                    />
+                    <span>Just Now</span>
+                </MessageInfoDiv>
+                <MessageContentDiv className="messageContent">
+                    <p>Hello</p>
+                    <img
+                        src={ImageExample}
+                        alt=""
+                    />
+                </MessageContentDiv>
+            </MessageContainerDiv>
+            <MessageContainerDiv className="">
+                <MessageInfoDiv>
+                    <img
+                        src={ImageExample}
+                        alt=""
+                    />
+                    <span>Just Now</span>
+                </MessageInfoDiv>
+                <MessageContentDiv className="messageContent">
+                    <p>Hello</p>
+                    <img
+                        src={ImageExample}
+                        alt=""
+                    />
+                </MessageContentDiv>
             </MessageContainerDiv>
         </>
     )
--- src/Components/Message/style.tsx
+++ src/Components/Message/style.tsx
@@ -4,4 +4,52 @@ export const MessageContainerDiv = styled.div`
     display: flex;
     gap: 20px;
     margin-bottom: 20px;
+    
+    &.owner {
+        flex-direction: row-reverse;
+        
+        .messageContent{
+            align-items: flex-end;
+        }
+
+        p {
+            background-color: ${props => props.theme.colors.message_BackgroundColorParagraphOwner};
+            color: ${props => props.theme.colors.message_TextColorParagraphOwner};
+            border-radius: 10px 0px 10px 10px;
+        }
+    }
+`;
+
+export const MessageInfoDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    font-weight: 300;
+    color: ${props => props.theme.colors.message_TextTimeSendMessage};
+    
+    img {
+        width: 35px;
+        height: 35px;
+        border-radius: 50%;
+        object-fit: cover;
+        border: 2px solid ${props => props.theme.colors.message_BorderImageProfile};
+    }
+`;
+
+export const MessageContentDiv = styled.div`
+    max-width: 80%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+    p {
+        color: ${props => props.theme.colors.message_TextColorParagraph};
+        background-color: ${props => props.theme.colors.message_BackgroundColorParagraph};
+        padding: 10px 20px;
+        border-radius: 0px 10px 10px 10px;
+        max-width: max-content;
+    }
+
+    img {
+        width: 50%;
+    }
 `;
