6787ae5232f5bd0055aa79433902c3c77a73d04c
LipeMachado <silvalipe765@gmail.com>
2022-10-18 17:34:45 -0300

    Added Messages

--- src/Components/Messages/style.tsx
+++ src/Components/Messages/style.tsx
@@ -3,6 +3,21 @@ import styled from 'styled-components'
 export const MessagesContainerDiv = styled.div`
     background-color: ${props => props.theme.colors.white};
     padding: 10px;
-    height: calc(100% - 100px);
+    height: calc(100% - 120px);
     overflow: scroll;
+    overflow-x: hidden;
+
+    &::-webkit-scrollbar {
+        width: 8px;
+    }
+
+    &::-webkit-scrollbar-track {
+        background: transparent;
+    }
+
+    &::-webkit-scrollbar-thumb {
+        background-color: #7923CF;
+        border-radius: 20px;
+        border: 3px solid white;
+    }
 `;
